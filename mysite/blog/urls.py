196caeddd73a66131851a63d4d84580ffee32cb4
Richard Brown <richardjbrown1@gmail.com>
2018-05-23 12:54:31 -0600

    Added Blog app and fixed some errors

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
+from django.views.generic import ListView, DetailView
+from blog.models import Post
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('personal.urls')),
-    path('blog/', include('blog.urls')),
-]
+urlpatterns = [ path('/', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],
+                                           template_name="blog/blog.html"))]
